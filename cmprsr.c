1c6fe6c1e0783c890e9bd67a60a25d134f532979
marcybelardo <marcelinebelardo@gmail.com>
2024-06-15 16:42:29 +0300

    Second try at doing it in C

--- cmprsr.c
+++ cmprsr.c
@@ -1,20 +1,14 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define ASCII_SIZE      256
-#define MAX_HEAP_SIZE   256
-#define MAX_CODE_LENGTH 256
-
-char *lookup_table[MAX_CODE_LENGTH];
-
-struct CharCount {
-	unsigned char character;
-	int count;
-};
+#define ASCII_SIZE 256
+#define MAX_HEAP_SIZE 256
+#define INTERNAL_NODE -1
 
 typedef struct HuffNode {
-	char c;
+	int c;
 	int weight;
 	struct HuffNode *l;
 	struct HuffNode *r;
@@ -35,7 +29,7 @@ void swap(HuffNode **a, HuffNode **b)
 /*
  *	HUFFMAN TREE IMPLEMENTATIONS
  */
-HuffNode *create_huff_node(char c, int weight)
+HuffNode *create_huff_node(int c, int weight)
 {
 	HuffNode *new = malloc(sizeof(HuffNode));
 	new->c = c;
@@ -49,6 +43,10 @@ MinHeap *create_min_heap()
 {
 	MinHeap *min_heap = malloc(sizeof(MinHeap));
 	min_heap->size = 0;
+
+	for (int i = 0; i < MAX_HEAP_SIZE; i++) {
+		min_heap->arr[i] = malloc(sizeof(HuffNode *));
+	}
 
 	return min_heap;
 }
@@ -65,18 +63,20 @@ void heapify(MinHeap *min_heap, int idx)
 	// We want to sort the array such that the smallest node is at the root
 	// So first we have to find the smallest element between the current set of
 	// nodes.
-	
+
 	// Check if the node is at an index currently outside the heap's registered size
 	// Then compare the weights of the node's left child and itself
 	// Pick the smallest value
-	if (l < min_heap->size && min_heap->arr[l]->weight < min_heap->arr[smallest]->weight) {
+	if (l < min_heap->size &&
+	    min_heap->arr[l]->weight < min_heap->arr[smallest]->weight) {
 		smallest = l;
 	}
 
 	// Check if the node is at an index currently outside the heap's registered size
 	// Then compare the weights of the node's right child and itself
 	// Pick the smallest value
-	if (r < min_heap->size && min_heap->arr[r]->weight < min_heap->arr[smallest]->weight) {
+	if (r < min_heap->size &&
+	    min_heap->arr[r]->weight < min_heap->arr[smallest]->weight) {
 		smallest = r;
 	}
 
@@ -104,7 +104,8 @@ void insert_min_heap(MinHeap *min_heap, HuffNode *node)
 	// Loop over the parents of each node, starting by comparing the new
 	// node and its parent (For a given node i, its parent is
 	// located at (i - 1) / 2)
-	while (i && min_heap->arr[i]->weight < min_heap->arr[(i - 1) / 2]->weight) {
+	while (i &&
+	       min_heap->arr[i]->weight < min_heap->arr[(i - 1) / 2]->weight) {
 		swap(&min_heap->arr[i], &min_heap->arr[(i - 1) / 2]);
 		i = (i - 1) / 2;
 	}
@@ -112,7 +113,8 @@ void insert_min_heap(MinHeap *min_heap, HuffNode *node)
 
 HuffNode *extract_min(MinHeap *min_heap)
 {
-	if (min_heap->size == 0) return NULL;
+	if (min_heap->size == 0)
+		return NULL;
 
 	HuffNode *tmp = min_heap->arr[0];
 
@@ -126,24 +128,22 @@ HuffNode *extract_min(MinHeap *min_heap)
 	return tmp;
 }
 
-HuffNode *build_huff_tree(struct CharCount *count_arr, int size)
+HuffNode *build_huff_tree(int *char_count)
 {
 	HuffNode *left, *right, *top;
 
 	MinHeap *min_heap = create_min_heap();
 
-	for (int i = 0; i < size; i++) {
-		insert_min_heap(
-			min_heap,
-			create_huff_node(count_arr[i].character, count_arr[i].count)
-		);
+	for (int i = 0; i < ASCII_SIZE; i++) {
+		if (char_count[i] > 0)
+			insert_min_heap(min_heap, create_huff_node(i, char_count[i]));
 	}
 
 	while (min_heap->size > 1) {
 		left = extract_min(min_heap);
 		right = extract_min(min_heap);
 
-		top = create_huff_node('$', left->weight + right->weight);
+		top = create_huff_node(-1, left->weight + right->weight);
 		top->l = left;
 		top->r = right;
 
@@ -162,175 +162,143 @@ void free_huff_tree(HuffNode *root)
 	}
 }
 
-/*
- *	ENCODING AND DECODING
- */
-void encode(HuffNode *root, char *code, int len)
+void generate_huffman_codes(HuffNode *node, int idx, char code[MAX_HEAP_SIZE], char *code_arr[MAX_HEAP_SIZE])
 {
-	if (!root) return;
-
-	// if node is a leaf, store it in the lookup table
-	if (!root->l && !root->r) {
-		code[len] = '\0';
-		lookup_table[(unsigned char)root->c] = strdup(code);
-		return;
-	}
-
-	// append '0' to the code and traverse the left subtree
-	code[len] = '0';
-	encode(root->l, code, len + 1);
-
-	// append '1' to the code and traverse the right subtree
-	code[len] = '1';
-	encode(root->r, code, len + 1);
-}
-
-/*
- *	CHAR ARRAYS AND SORTING
- */
-static int create_char_array(int *char_count, struct CharCount *count_arr)
-{
-	int index = 0;
-
-	for (int i = 0; i < ASCII_SIZE; i++) {
-		if (char_count[i] > 0) {
-			count_arr[index].character = (unsigned char)i;
-			count_arr[index].count = char_count[i];
-			index++;
+	if (node->c != INTERNAL_NODE) {
+		code[idx] = '\0';
+		code_arr[node->c] = strdup(code);
+	} else {
+		if (node->l) {
+			code[idx] = '0';
+			generate_huffman_codes(node->l, idx + 1, strdup(code), code_arr);
+		}
+		if (node->r) {
+			code[idx] = '1';
+			generate_huffman_codes(node->r, idx + 1, strdup(code), code_arr);
 		}
 	}
-
-	return index;
 }
 
-int compare(const void *a, const void *b)
+void read_from_file(const char *filename, int char_count[ASCII_SIZE],
+		    unsigned char **buf)
 {
-	struct CharCount *char_count_a = (struct CharCount *)a;
-	struct CharCount *char_count_b = (struct CharCount *)b;
-	return char_count_a->count - char_count_b->count;
-}
+	FILE *fp = fopen(filename, "r");
+	if (fp == NULL) {
+		perror("Failed to open file");
+		exit(EXIT_FAILURE);
+	}
 
-static void sort_char_count_array(struct CharCount *count_arr, int size)
-{
-	qsort(count_arr, size, sizeof(struct CharCount), compare);
+	size_t capacity = 1024;
+	*buf = (unsigned char *)malloc(capacity);
+	if (!*buf) {
+		perror("Failed to allocate memory");
+		fclose(fp);
+		exit(EXIT_FAILURE);
+	}
+
+	size_t buf_size = 0;
+	int c;
+	while ((c = fgetc(fp)) != EOF) {
+		if (buf_size >= capacity) {
+			capacity *= 2;
+			*buf = (unsigned char *)realloc(*buf, capacity);
+			if (!*buf) {
+				perror("Failed to reallocate memory");
+				fclose(fp);
+				exit(EXIT_FAILURE);
+			}
+		}
+		(*buf)[buf_size++] = (unsigned char)c;
+
+		if (c >= 0 && c < ASCII_SIZE)
+			char_count[(unsigned char)c]++;
+	}
+	(*buf)[buf_size] = '\0';
+
+	fclose(fp);
+
+	printf("%s\n\n", *buf);
 }
 
 /*
  *	MAIN
  */
+
+static int encode_f;
+
 int main(int argc, char **argv)
 {
-	char *filename;
-	char *buf;
-	size_t buf_size = 0;
-	size_t buf_capacity = 1;
+	char *infile = NULL;
+	char *outfile = NULL;
+	int c;
 
-	buf = (char *)malloc(buf_capacity * sizeof(char));
-	if (!buf) {
-		perror("Failed to allocated memory");
-		return EXIT_FAILURE;
+	while (1) {
+		static struct option long_options[] =
+		{
+			{"encode", no_argument, &encode_f, 1},
+			{"decode", no_argument, &encode_f, 0},
+			{"file", required_argument, 0, 'f'},
+			{"out", required_argument, 0, 'o'},
+			{0, 0, 0, 0}
+		};
+
+		int opt_idx = 0;
+		c = getopt_long(argc, argv, "f:o:", long_options, &opt_idx);
+
+		if (c == -1) break;
+
+		switch (c) {
+			case 0:
+				if (long_options[opt_idx].flag != 0) break;
+				printf("option %s", long_options[opt_idx].name);
+				if (optarg)
+					printf(" with arg %s", optarg);
+				printf("\n");
+				break;
+			case 'f':
+				infile = optarg;
+				break;
+			case 'o':
+				outfile = optarg;
+				break;
+			case '?':
+				break;
+			default:
+				abort();
+		}
 	}
-	
-	int char_count[ASCII_SIZE] = {0};
 
-	if (argc != 2) {
-		perror("Incorrect number of arguments");
-		return EXIT_FAILURE;
+	if (encode_f) {
+		// ENCODE
+		printf("Encoding...\n");
+
+		int char_count[ASCII_SIZE] = { 0 };
+		char code[MAX_HEAP_SIZE] = { 0 };
+		char *code_arr[MAX_HEAP_SIZE];
+		
+		unsigned char *buf;
+		read_from_file(infile, char_count, &buf);
+
+		HuffNode *root = build_huff_tree(char_count);
+
+		for (int i = 0; i < MAX_HEAP_SIZE; i++) {
+			code_arr[i] = malloc(MAX_HEAP_SIZE);
+		}
+
+		generate_huffman_codes(root, 0, code, code_arr);
+
+		for (int i = 0; i < MAX_HEAP_SIZE; i++) {
+			if (strlen(code_arr[i]) > 0)
+				printf("%c (0x%x): %s\n", (char)i, i, code_arr[i]);
+		}
+
+		free_huff_tree(root);
+		free(buf);
 	} else {
-		filename = argv[1];
+		// DECODE
+		printf("Decode flag set!\n");
 	}
 
-	FILE *fp;
-	char c;
-
-	fp = fopen(filename, "r");
-	if (fp == NULL) {
-		perror("Failed to open file");
-		return 1;
-	}
-
-	while ((c = fgetc(fp)) != EOF) {
-		char_count[(unsigned char)c]++;
-
-		if (buf_size + 1 >= buf_capacity) {
-			buf_capacity *= 2;
-			buf = (char *)realloc(buf, buf_capacity * sizeof(char));
-			if (!buf) {
-				perror("Failed to reallocate memory");
-				fclose(fp);
-				return 1;
-			}
-		}
-
-		buf[buf_size++] = c;
-	}
-
-	buf[buf_size] = '\0';
-
-	fclose(fp);
-
-	printf("%s\n\n", buf);
-
-	int non_zero = 0;
-	for (int i = 0; i < ASCII_SIZE; i++) {
-		if (char_count[i] > 0) non_zero++;
-	}
-
-	struct CharCount *char_count_arr = (struct CharCount *)malloc(non_zero * sizeof(struct CharCount));
-	int valid_counts = create_char_array(char_count, char_count_arr);
-
-	sort_char_count_array(char_count_arr, valid_counts);
-
-	// HUFFMAN CODING TREE ALGORITHM
-	//
-	// 1. Take the lowest two nodes, get the sum of their weights
-	// 2. Set left node to the lower w val, right node to the higher w val
-	// 3. Re-sort list with the new tree node introduced
-	// 4. Continue with the next two lowest nodes
 	
-	for (int i = 0; i < ASCII_SIZE; i++) {
-		if (char_count_arr[i].count > 0) {
-			printf(
-				"CHAR: %c (0x%02x): COUNT: %d\n",
-				char_count_arr[i].character,
-				char_count_arr[i].character,
-				char_count_arr[i].count
-			);
-		}
-	}
-	
-	HuffNode *root = build_huff_tree(char_count_arr, valid_counts);
-
-	for (int i = 0; i < MAX_CODE_LENGTH; i++) {
-		lookup_table[i] = NULL;
-	}
-
-	char code[MAX_CODE_LENGTH];
-	encode(root, code, 0);
-
-	FILE *out_fp = fopen("out.cmpr", "wb");
-	if (!out_fp) {
-		perror("Failed to open file.");
-		return EXIT_FAILURE;
-	}
-
-	for (int i = 0; i < strlen(buf); i++) {
-		printf("%s", lookup_table[(unsigned char)buf[i]]);
-
-		char *huff_code = lookup_table[(unsigned char)buf[i]];
-		if (huff_code) fputs(huff_code, out_fp);
-	}
-	printf("\n");
-
-	fclose(out_fp);
-
-	for (int i = 0; i < MAX_CODE_LENGTH; i++) {
-		free(lookup_table[i]);
-	}
-
-	free_huff_tree(root);
-	free(buf);
-
 	return EXIT_SUCCESS;
 }
-
